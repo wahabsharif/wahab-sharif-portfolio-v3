138502d19a3a041f6e5d3847df5d14dedd876f87
Muhammad Wahab Sharif <www.rex19@gmail.com>
2024-03-21 01:47:41 +0500

    vercel analytics and speed insight

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,11 @@
 import MainLayout from "@/layout/MainLayout";
 import "@/styles/globals.css";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { AnimatePresence } from "framer-motion";
 import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
 import { useRouter } from "next/router";
-// import { SpeedInsights } from "@vercel/speed-insights/next";
 
 // Import the types for Next.js
 /// <reference types="@vercel/next/global" />
@@ -22,7 +22,7 @@ export default function App({ Component, pageProps }: AppProps) {
         </MainLayout>
       </ThemeProvider>
       <Analytics />
-      {/* <SpeedInsights /> */}
+      <SpeedInsights />
     </>
   );
 }
