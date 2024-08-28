98c5ea00c8ecd0f764f403ceb7f9e9abaca9b03b
Muhammad Wahab Sharif <www.rex19@gmail.com>
2024-08-28 17:18:14 +0500

    Bug Fixed

--- src/data/education.ts
+++ src/data/education.ts
@@ -3,34 +3,37 @@ import { TShowCaseListDetails } from "@/components/ShowCaseList";
 export const EDUCATION: TShowCaseListDetails[] = [
   {
     title: "Bachelor Of Engineering in Electronics and Telecommunication",
-    organisation: {
+    organization: {
       name: "Rizvi College Of Engineering",
       href: "https://eng.rizvi.edu.in/",
     },
     date: "2019-2023",
     location: "Bandra, Mumbai",
+    review: "",
     description:
       "Relevant courses included Data Structures and Algorithms, Web Design, Cloud Computing, Artificial Neural Network and Database Management.",
   },
   {
     title: "Higher Secondary School",
-    organisation: {
+    organization: {
       name: "Mithibai College",
       href: "https://www.mithibai.ac.in/",
     },
     date: "2016-2018",
     location: "Vile Parle, Mumbai",
+    review: "",
     description:
       "Completed the course in Electronics Bifocal Science with 68%. Started freelancing making 3D assets for games. Got interested in game development and learned unity and c#",
   },
   {
     title: "Secondary School Education",
-    organisation: {
+    organization: {
       name: "Holy Family High School",
       href: "https://holyfamilyandheri.org/",
     },
     date: "2017-2018",
     location: "Andheri, Mumbai",
+    review: "",
     description:
       "Learned good values,dedication and disciplne. Played district football for 5 years.",
   },
